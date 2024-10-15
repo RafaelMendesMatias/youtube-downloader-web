72294ea8b16a74ebb18a2bc13eb422e1785684d3
RafaelMendesMatias <rfmend85@gmail.com>
2024-10-15 13:58:33 -0300

    .

--- main.py
+++ main.py
@@ -4,9 +4,10 @@ from pytubefix.cli import on_progress
 url = "https://www.youtube.com/watch?v=a6991rC2DgA&list=RDa6991rC2DgA&start_radio=1"
  
 yt = YouTube(url, on_progress_callback = on_progress)
-print(yt.vid_details)
 
-print(dir(yt))
- 
-# ys = yt.streams.get_highest_resolution()
-# ys.download()
+# print(yt.streams)
+
+
+for stream in yt.streams:
+    if stream.type == "video" and hasattr(stream, "res"):
+        print(f"Resolução: {stream.res}, Codec de vídeo: {stream.vcodec}, Codec de áudio: {stream.acodec}")
