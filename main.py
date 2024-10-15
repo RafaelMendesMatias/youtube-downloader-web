51ed4277ddf417c60523a849c3e81046f8bd2414
RafaelMendesMatias <rfmend85@gmail.com>
2024-10-15 14:39:55 -0300

    .

--- main.py
+++ main.py
@@ -4,9 +4,9 @@ from pytubefix.cli import on_progress
 url = "https://www.youtube.com/watch?v=a6991rC2DgA&list=RDa6991rC2DgA&start_radio=1"
  
 yt = YouTube(url, on_progress_callback = on_progress)
-print(yt.vid_details)
+print(yt.title)
 
-print(dir(yt))
+# print(vars(yt))
  
 # ys = yt.streams.get_highest_resolution()
 # ys.download()
